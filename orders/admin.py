44f629eda33eda1bbece1d605a1e3031f2150e3c
aayushiie <aayushi.singh067@gmail.com>
2026-03-10 21:53:58 +0530

    new branch

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import FoodCourt, MenuItem, Order
 
-# Register your models here.
+admin.site.register(FoodCourt)
+admin.site.register(MenuItem)
+admin.site.register(Order)
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class FoodCourt(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    foodcourt = models.ForeignKey(FoodCourt, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20, default="Pending")
