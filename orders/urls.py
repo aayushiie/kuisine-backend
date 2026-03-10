a6856908c0dbd933bb6fd8193d7256b372b89961
aayushiie <aayushi.singh067@gmail.com>
2026-03-10 21:55:31 +0530

    update orders

--- orders/urls.py
+++ orders/urls.py
@@ -3,9 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('foodcourts/', views.get_foodcourts),
-    path('menu/', views.get_menu),
-    path('create/', views.create_order),
-    path('status/<int:order_id>/', views.order_status),
+    
 
 ]
